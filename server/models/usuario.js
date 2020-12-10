b622c01d2b94c5a6db23407f03d3903dc6664f01
Neftali12 <neftiie.carranza@gmail.com>
2020-12-10 13:43:37 -0600

    DAPS-AWOS

---
+++ server/models/usuario.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+let usuarioSchema = new Schema({
+    _id: {
+        type: String,
+        required: [true, 'El id es necesario']
+    },
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es necesario']
+    },
+    apellidos: {
+        type: String,
+        required: [true, 'El nombre es necesario']
+    },
+    email: {
+        type: String,
+        required: [true, 'El correo es necesario'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'La contraseña es necesaria']
+    },
+    img: {
+        type: String,
+        required: false
+    },
+    role: {
+        type: String,
+        default: 'USER_ROLE'
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    }
+});
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
---
+++ server/server.js
@@ -0,0 +1,49 @@
+require('./config/config');
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+var bodyParser = require('body-parser');
+// Habilita CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+    );
+    next();
+});
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+app.get('/', function(req, res) {
+    res.send('<h1>Equpipo 2 -4A </h1>');
+});
+
+app.use(require('./routes/usuario'));
+app.use(require('./routes/categoria'));
+app.use(require('./routes/producto'));
+app.use(require('./routes/login'));
+
+mongoose.connect('mongodb+srv://admin:administrador15@cluster0.29r8p.mongodb.net/cafeteria', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}, (err, res) => {
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('La aplicacion esta en linea por el puerto', process.env.PORT)
+});
