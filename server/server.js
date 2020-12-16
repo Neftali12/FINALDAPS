cdea4063b296579d76b4a64e932630cc6c6df09d
Neftali12 <neftiie.carranza@gmail.com>
2020-12-15 21:47:10 -0600

    HELLO INTER 2.0

--- server/server.js
+++ server/server.js
@@ -49,56 +49,8 @@ app.use(require('./routes/login'));
 
 
 //HOLAMUNDO
-mongoose.connect('require('. / config / config ')
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const app = express();
-
-//CORS
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-    res.setHeader(
-        'Access-Control-Allow-Methods',
-        'GET, POST, PATCH, PUT, DELETE, OPTIONS'
-    );
-    next();
-});
-
-//hola mundo 
-//parse application/x-www-fomr-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-
-//parse application/json
-app.use(bodyParser.json());
-
-
-// app.use(cors());
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept');
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE, PATCH');
-//     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE, PATCH');
-//     next();
-// });
-
-
-
-app.get('/', function(req, res) {
-    res.send('<h1>Bienvenido a mi servidor REST</h1>');
-});
-
-app.use(require('./routes/usuario')); app.use(require('./routes/categoria')); app.use(require('./routes/producto')); app.use(require('./routes/login'));
-
-
-//HOLAMUNDO
-//mongoose.connect('mongodb +srv://admin:12345678__@cluster0.zrrea.mongodb.net/cafeteria', {
-mongoose.connect('mongodb://localhost/cafeteria', {
+mongoose.connect('mongodb+srv://admin:administrador15@cluster0.29r8p.mongodb.net/cafeteria', {
+    // mongoose.connect('mongodb://localhost/cafeteria', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
@@ -106,20 +58,6 @@ mongoose.connect('mongodb://localhost/cafeteria', {
 }, (err, res) => {
     if (err) throw err;
     console.log('Base de datos ONLINE');
-});
-
-app.listen(process.env.PORT, () => {
-    console.log('El servidor esta en linea en el puerto', process.env.PORT);
-});
-', {
-// mongoose.connect('mongodb://localhost/cafeteria', {
-useNewUrlParser: true,
-useUnifiedTopology: true,
-useFindAndModify: false,
-useCreateIndex: true
-}, (err, res) => {
-if (err) throw err;
-console.log('Base de datos ONLINE');
 });
 
 app.listen(process.env.PORT, () => {
